b3f47090e357c7fbc4820b6db84d20f49cb9e649
Sebastian-Al <sebastian_al95@hotmail.com>
2023-03-03 02:28:35 -0300

    Se agrego carrusel, seccion educacion y formulario

--- js/function.js
+++ js/function.js
@@ -1,20 +1,43 @@
+//Boton para ir al principio de la pagina
 window.addEventListener('scroll', function() {
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    var scrollToTopBtn = document.getElementById('scroll-to-top-btn');
-    
-    if (scrollPosition > 0) {
-      scrollToTopBtn.style.display = 'block';
-    } else {
-      scrollToTopBtn.style.display = 'none';
-    }
-  });
-  
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
-  
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   var scrollToTopBtn = document.getElementById('scroll-to-top-btn');
-  scrollToTopBtn.addEventListener('click', scrollToTop);
+  
+  if (scrollPosition > 0) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+var scrollToTopBtn = document.getElementById('scroll-to-top-btn');
+scrollToTopBtn.addEventListener('click', scrollToTop);
+
+//Carrusel
+let slideIndex = 0;
+showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("Slides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000); // Cambia de imagen cada 3seg
+  };
+
+  // Formulario
+modal = document.getElementById('contactForm');
+// Cuando se hace click fuera del formulario se cierra
+window.onclick = function(event) {if (event.target == modal) {modal.style.display = "none";}};
+
